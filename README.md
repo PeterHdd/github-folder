f17ff37b234c9cd9c6361e7ed0dc4ca5745ba6b9
PeterHdd <peterhd3412@gmail.com>
2019-06-09 19:58:51 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# GITHUB FOLDER
+## Automtically generate the .github folder
+-------------------
+## Information
+
+This vscode extension will automtically create a .github folder that will contain the issue template files, codeowner file and code of conduct file.
+
+**Note:** Don't forget to add your username to the specific files.
+
+![github-folder](https://media.giphy.com/media/JRCnf848hWediiYPkz/giphy.gif)
---
+++ src/extension.ts
@@ -0,0 +1,49 @@
+import { commands, workspace, ExtensionContext } from 'vscode';
+import * as fs from 'fs';
+import * as path from 'path';
+import * as mkdirp from 'mkdirp';
+import * as data from './data';
+
+
+export function activate(context: ExtensionContext) {
+
+		console.log('Congratulations, your extension "github-folder" is now active!');
+
+	let disposable = commands.registerCommand('extension.github', (context) => {
+
+		const githubPath   = path.join(workspace.rootPath as string, '.github');
+		const templatePath = path.join(githubPath, 'ISSUE_TEMPLATE');
+		mkdirp(templatePath,(err) => {
+			if (err)
+			throw err;
+			else
+			{
+			fs.writeFile(templatePath +'/bug_report.md',data.bugData, (err) => {
+				if (err) throw err;
+				console.log(err);
+				console.log('The file has been saved!');
+				});
+			fs.writeFile(templatePath +'/feature_request.md',data.featureData, (err) => {
+				if (err) throw err;
+				console.log(err);
+				console.log('The file has been saved!');
+				});
+			fs.writeFile(githubPath +'/CODEOWNERS',data.codeOwners, (err) => {
+				if (err) throw err;
+				console.log(err);
+				console.log('The file has been saved!');
+				});
+			fs.writeFile(githubPath +'/CODE_OF_CONDUCT.md',data.codeOFConduct, (err) => {
+				if (err) throw err;
+				console.log(err);
+				console.log('The file has been saved!');
+				});
+			}
+		  });
+	});
+
+	context.subscriptions.push(disposable);
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {}
